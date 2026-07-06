5f4e09101e2b000b999ecd8c6ba492c565aed322
Andrés Tobelem <andres.tobelem@gmail.com>
2026-07-06 02:10:35 -0300

    refactor(pandi-dynamic-workflows): extract public api facade

---
+++ extensions/pandi-dynamic-workflows/workflow-public-api.ts
@@ -0,0 +1,82 @@
+export { liveAgentHeaderStatus } from "./agent-view.js";
+export { countRunArtifacts } from "./dashboard-collectors.js";
+export {
+	booleanValue,
+	formatAgentPhase,
+	getAgentElapsedMs,
+	isAgentMonitorState,
+	mergeAgentMonitor,
+	numberValue,
+	phaseEventFields,
+	readRunEvents,
+	recordValue,
+	stringArrayValue,
+	stringValue,
+} from "./event-parser.js";
+export { appendFileMutexCount, appendJsonLine } from "./file-append.js";
+export { runProcess, runStreamingAgentProcess } from "./process-spawn.js";
+export { settleWithinTimeout } from "./run-lifecycle.js";
+export {
+	activeRunCount,
+	activeRunIds,
+	clearActiveRuns,
+	getActiveRun,
+	hasActiveRun,
+	listActiveRuns,
+	registerActiveRun,
+	unregisterActiveRun,
+} from "./run-registry.js";
+export { estimatePeakParallelAgents } from "./run-state.js";
+export { selectRunByKey } from "./run-view.js";
+export {
+	EXTENSION_ROOT,
+	JOURNAL_FILE,
+	MAX_AGENT_OUTPUT_IN_RESULT,
+	MAX_JOURNALED_STREAM,
+	PI_SESSION_HEARTBEAT_MS,
+	PROCESS_KILL_GRACE_MS,
+} from "./runtime-constants.js";
+export type {
+	ActiveWorkflowRun,
+	AgentMonitorModel,
+	AgentMonitorState,
+	AgentOptions,
+	AgentPhaseInfo,
+	AskResult,
+	BashResult,
+	DynamicWorkflowAction,
+	DynamicWorkflowToolParams,
+	JournalCache,
+	JournalRecord,
+	PreparedWorkflowRun,
+	RunLimits,
+	SubagentResult,
+	WorkflowDefinition,
+	WorkflowFile,
+	WorkflowLocation,
+	WorkflowLogEntry,
+	WorkflowResultIntegrity,
+	WorkflowRunRecord,
+	WorkflowRunResult,
+	WorkflowRunState,
+	WorkflowRunStatus,
+	WorkflowScope,
+	WorkflowScopeInput,
+} from "./types.js";
+export { extractUltracodeTask } from "./ultracode.js";
+export { currentWorkflowDepth, maxWorkflowDepth } from "./workflow-depth.js";
+export { runWorkflow } from "./workflow-engine.js";
+export { resolveWorkflowMenu } from "./workflow-menu.js";
+export {
+	formatWorkflowPreflightSummary,
+	preflightWorkflowLaunch,
+	type WorkflowPreflightResult,
+} from "./workflow-preflight.js";
+export {
+	WORKFLOW_DIR,
+	WORKFLOW_DRAFT_DIR,
+	WORKFLOW_GRAPH_DIR,
+	WORKFLOW_RUN_DIR,
+} from "./workflow-resolve.js";
+export { prepareWorkflowRun } from "./workflow-run-prepare.js";
+export { transformWorkflowCode } from "./workflow-transform.js";
